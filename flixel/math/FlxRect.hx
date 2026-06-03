dc477b80d1fd7fe88b817541f7a78370c39f3051
TracedInPurple <tiagoonyt@gmail.com>
2026-06-03 22:44:31 +0200

    Added 2 new Functions for FlxRect

--- flixel/math/FlxRect.hx
+++ flixel/math/FlxRect.hx
@@ -236,6 +236,44 @@ class FlxRect implements IFlxPooled
 	}
 
 	/**
+	 * Returns the area of intersection with specified rectangle.
+	 * If the rectangles do not intersect, this method returns an empty rectangle.
+	 *
+	 * @param   rect    Rectangle to check intersection against
+	 * @param   result  The resulting instance, if `null`, a new one is created
+	 * @return  The area of intersection of two rectangles
+	 */
+	public function intersection(rect:FlxRect, ?result:FlxRect):FlxRect
+	{
+		if (result == null)
+			result = FlxRect.get();
+		
+		final x0:Float = x < rect.x ? rect.x : x;
+		final x1:Float = right > rect.right ? rect.right : right;
+		final y0:Float = y < rect.y ? rect.y : y;
+		final y1:Float = bottom > rect.bottom ? rect.bottom : bottom;
+		rect.putWeak();
+		
+		if (x1 <= x0 || y1 <= y0)
+			return result.set(0, 0, 0, 0);
+		
+		return result.set(x0, y0, x1 - x0, y1 - y0);
+	}
+
+	/**
+	 * Resizes `this` instance so that it fits within the intersection of the this and
+	 * the target rect. If there is no overlap between them, The result is an empty rect.
+	 *
+	 * @param   rect    Rectangle to check intersection against
+	 * @return  This rect, useful for chaining
+	 * @since 5.9.0
+	 */
+	public function clipTo(rect:FlxRect):FlxRect
+	{
+		return rect.intersection(this, this);
+	}
+
+	/**
 	 * Returns true if this FlxRect contains the FlxPoint
 	 *
 	 * @param   xPos  The x position to check
